494172ec7eb0b251d7168c21d979477afc548c32
M.A.M.A.T.H.A <95341333+Mama-gama@users.noreply.github.com>
2024-05-10 15:36:34 +0530

    Add files via upload

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="dashboard">
+		<DashboardInfo />
+		<DashboardContent />
+	</div>
+</template>
+
+<script>
+	import { mapGetters, mapActions } from "vuex";
+	import DashboardInfo from "@/components/Dashboard/DashboardInfo.vue";
+	import DashboardContent from "@/components/Dashboard/DashboardContent.vue";
+
+	export default {
+		"name": "Dashboard",
+		"components": {
+			DashboardInfo,
+			DashboardContent,
+		},
+		"computed": {
+			...mapGetters([
+				"getTaskList",
+			]),
+		},
+		"watch": {
+			"getTaskList": {
+				handler() {
+					this.createTaskListLocalStorage();
+				},
+				"deep": true,
+			},
+		},
+		mounted() {
+			this.checkTaskListLocalStorage();
+		},
+		"methods": {
+			...mapActions([
+				"checkTaskListLocalStorage",
+				"createTaskListLocalStorage",
+			]),
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.dashboard {
+		position: relative;
+		width: 80%;
+		max-width: 50rem;
+		margin: 0 auto 3rem;
+		overflow: hidden;
+		transition: 0.5s ease-in-out 0s;
+		background-color: $color-white;
+		box-shadow: -0.2rem 0.2rem 0.2rem -0.1rem rgba($color-black, 0.15);
+
+		@include media("md") {
+			width: 100%;
+			max-width: inherit;
+		}
+	}
+</style>
---
+++ src/components/Dashboard/DashboardInfo.vue
@@ -0,0 +1,103 @@
+<template>
+	<div class="dashboard-info">
+		<ul class="dashboard-info__list">
+			<li class="dashboard-info__item">
+				<UITag
+					tag-name="Tasks"
+					:tag-value="totalTaskList"
+				/>
+			</li>
+			<li class="dashboard-info__item">
+				<UITag
+					tag-name="Tasks Done"
+					:tag-value="totalTaskListDone"
+				/>
+			</li>
+		</ul>
+		<ul class="dashboard-info__list">
+			<li
+				v-if="totalTaskListDone !== 0"
+				class="dashboard-info__item"
+			>
+				<UIButton
+					class="button--bg-color-error button--small"
+					@button-click="removeTaskListDone()"
+				>
+					<span class="button__icon">
+						<i class="icon">
+							<FontAwesomeIcon icon="trash" />
+						</i>
+					</span>
+					<span class="button__text">
+						tasks done
+					</span>
+				</UIButton>
+			</li>
+			<li class="dashboard-info__item">
+				<UIButton
+					class="button--bg-color-error button--small"
+					@button-click="removeTaskList()"
+				>
+					<span class="button__icon">
+						<i class="icon">
+							<FontAwesomeIcon icon="trash" />
+						</i>
+					</span>
+					<span class="button__text">
+						tasks
+					</span>
+				</UIButton>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	import { mapGetters, mapActions } from "vuex";
+	import UIButton from "@/components/UI/UIButton.vue";
+	import UITag from "@/components/UI/UITag.vue";
+
+	export default {
+		"name": "DashboardInfo",
+		"components": {
+			UIButton,
+			UITag,
+		},
+		"computed": {
+			...mapGetters({
+				"totalTaskList": "getTotalTaskList",
+				"totalTaskListDone": "getTotalTaskListDone",
+			}),
+		},
+		"methods": {
+			...mapActions([
+				"removeTaskList",
+				"removeTaskListDone",
+			]),
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.dashboard-info {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-end;
+		width: 100%;
+		padding: 0.5rem;
+		background-color: $color-ghost;
+		font-size: 1.4rem;
+
+		&__list {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: flex-end;
+			list-style: none;
+		}
+
+		&__item {
+			margin: 0.5rem;
+		}
+	}
+</style>
