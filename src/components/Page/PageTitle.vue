112e73dfa79f1217396d052a934b1607b4cff090
M.A.M.A.T.H.A <95341333+Mama-gama@users.noreply.github.com>
2024-05-10 15:38:13 +0530

    Add files via upload

---
+++ src/components/Page/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+	<h1 class="page-title">
+		{{ title }}
+	</h1>
+</template>
+
+<script>
+	export default {
+		"name": "PageTitle",
+		"props": {
+			"title": {
+				"type": String,
+				"required": true,
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.page-title {
+		margin-bottom: 2rem;
+		color: $color-white;
+		font-family: $font-brand-1;
+		font-size: 6.4rem;
+		font-weight: 600;
+		line-height: 100%;
+		text-align: center;
+
+		@include media("sm") {
+			font-size: 12vw;
+		}
+	}
+</style>
